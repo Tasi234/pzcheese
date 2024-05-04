4599d76b3a3f6afe6842bafa3531887e2cc3dd08
Tasi234 <66006824+Tasi234@users.noreply.github.com>
2024-05-04 17:36:59 +1000

    Commenting and Readme Update 04/05/24

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Pzcheese
 
+Simple POC of a front end that displays stocked items for a fictional Cheeseria. 
+Product data is stored within the 'dataAPI' index file in a simple array and is filtered through an Angular interface via a Angular service and pushed to a simple html page. 
 
+## To-Do Items
+a>	Flesh out html and css
+b>	Continue to build out reactive forms or similar as required for calculator function, right now calculator non-functional.
+
+## Challenges and Successes
+a> Understanding angular modules and imports still a weak point for me
+b> Building the API and component functionality easy enough once I got back into the swing of things
+c> Reading HTML is easy enough, creating it from scratch is not a strong suit, and getting ChatGPT to generate it all doesn't prove my skillset. 
+d> Have never used (or even heard of tbh) Docker before, will have to do some homework if this is a suite used irl. 
+e> Spent far too much time troubleshooting HttpClientModule import errors. 
 
 ## Inspo
 
-Code used was taken from my uni major project and
+Code used was inspired by my uni major project and
 https://stackademic.com/blog/fetching-data-from-an-api-in-angular
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, Validators, FormGroup, FormControl, NgForm, FormsModule } from '@angular/forms';
+
 
 import { CheeseService } from './cheese.service'
 import { Cheese } from './cheese.interface'
@@ -15,12 +17,40 @@ import { Cheese } from './cheese.interface'
 export class AppComponent implements OnInit {
   title = 'pzcheese';
   cheeseList: Cheese[] = [];
+  //create a variable to the spec defined in the cheese interface 
+
+  qtyCalcForm: FormGroup = new FormGroup({
+    qtyReq: new FormControl(null),
+    qtySub: new FormControl(null)
+  });
+
 
   constructor(private cheeseService: CheeseService) {}
 
   ngOnInit(): void {
+    //on page load call the api and pull the data to fill
     this.cheeseService.getCheese().subscribe((cheeseList) => {
       this.cheeseList = cheeseList;
     });
   }
+
+  math: number = 0;
+  qtyCalc() {
+    const qtyMath: number = 
+      this.qtyCalcForm.value.qtyReq * 123;
+      //calculator functionality flawed as there must be an easier way to manage 
+      //the inputs needed to get the math done correctly and have it lay out in 
+      //table in an apprioprate fashion. 
+
+  }
+
+  // testResult: boolean = false;
+  // unitTest1() {
+  //   if (this.math == this.cheeseList([1][4])) {
+  //     this.testResult = true 
+  //   } else {
+  //     this.testResult = false;    }
+  //   }
+
+  //unit test for calculator function - fail. 
 }
